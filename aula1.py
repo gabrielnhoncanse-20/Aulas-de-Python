28cc4940fba4881b167d7bdd7c861ea7a2b7aab6
gabrielnhoncanse-20 <gabrielbastosnhoncanse@gmail.com>
2025-03-24 21:31:38 -0300

    Update aula1.py

--- aula1.py
+++ aula1.py
@@ -4,3 +4,83 @@ b=8.5
 c=7
 m=((a+b+c))/3
 print(m)
+
+Aula2
+
+# a=3
+# b=5
+# print('2a*3b=',2*a*3*b)
+# c=9
+# print(a+b+c)
+#
+# print(type(a))
+
+# numero=float(round(3.3224,2))
+# #exemplo:1
+# print("número com duas casas decimas é",numero,'.')
+# print("número com duas casas decimas é"+str(numero)+'.')
+# print("número com duas casas decimas é",str(numero)+'.')
+# #exemplo:2
+# print("número com duas casas decimas é %.2f." %numero)
+# #exemplo;3
+# print("número com duas casas decimas é {:.2f}.".format(numero))
+# #exenplo:4
+# print(f"número com duas casas decimas é {numero:.2f}.")
+#
+# nome=input('Nome:')
+# sobre=input('Sobrenome:')
+# print(f'Seu nome é: {nome} {sobre}')
+
+# a=5+5
+# print(5+5)
+# print(7-2)
+# print(2*2)
+# print(4/2)
+# print(10%3)
+# print(4**2)
+# print(10//3)
+
+# nota=8
+# media=7
+# aprovado=nota>=media
+# print(aprovado)
+#
+# nome_comp=input('Nome completo: ')
+# idade=int(input('idade: '))
+# altura=float(input('Altuea: '))
+# print(nome_comp)
+# print(idade)
+# print(altura)
+
+# anos=int(input('anos de serviço:'))
+# valor=float(input('valor por anos:'))
+# bonus=anos*valor
+# print('Bôns em R$:',bonus)
+
+#exercicio:1
+# num1=int(input("Numero 1:"))
+# num2=int(input('Numero 2:'))
+# soma=num1+num2
+# print('A soma é:', soma)
+
+#exercicio:2
+# metro=int(input('numero em metros:'))
+# a1=metro*(10**3)
+# print('metos em milímetros:', a1)
+
+#exercicio:3
+# dia=int(input('dias:'))
+# hora=int(input('horas:'))
+# min=int(input('minutos:'))
+# seg=int(input('segundos:'))
+# a1=dia*24
+# a2=hora*60*a1
+# a3=min*60*a2+seg
+# print(a3)
+
+#exercicio:4
+#salario1=int(input('salario:'))
+#por=int(input('porcentagem:'))
+#a1=1+(10**-2*por)
+#a2=salario1*a1
+#print('novo salario:',a2)
